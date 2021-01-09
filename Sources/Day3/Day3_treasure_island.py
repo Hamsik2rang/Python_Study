0bcc323e77b0b5f010d8dc99566ea4044bb31696
Im Yongsik <lvhi7121@gmail.com>
2021-01-09 20:48:56 +0900

    string lower 추가

--- Sources/Day3/Day3_treasure_island.py
+++ Sources/Day3/Day3_treasure_island.py
@@ -29,19 +29,19 @@ print("Your mission is to find the treasure.")
 
 choice = input(
     "You're at a cross road. Where do you want to go? Type 'left' or 'right'\n"
-)
+).lower()
 
 if choice == "left":
     choice = input(
         "You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. or Type 'swim' to swim across.\n"
-    )
+    ).lower()
 
     if choice == "swim":
         print("You attaked by piranhas. Game Over.")
     else:
         choice = input(
             "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?\n"
-        )
+        ).lower()
 
         if choice == "red":
             print("You burnd by flame. Game Over.")
