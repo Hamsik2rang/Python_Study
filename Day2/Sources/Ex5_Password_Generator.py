fde98c630fb426831720b0f16d5fd608cc1b1d21
Im Yongsik <lvhi7121@gmail.com>
2021-01-10 21:08:49 +0900

    Modify comment

--- Day2/Sources/Ex5_Password_Generator.py
+++ Day2/Sources/Ex5_Password_Generator.py
@@ -62,7 +62,7 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# Eazy Level - Order not randomised:
+# Order not randomized:
 # e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 pw = ""
 
@@ -74,7 +74,7 @@ for _ in range(0, nr_symbols):
     pw += random.choice(symbols)
 
 print("Here is your password:", pw)
-# Hard Level - Order of characters randomised:
+# Order of characters randomized:
 # e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
 # use random.shuffle
