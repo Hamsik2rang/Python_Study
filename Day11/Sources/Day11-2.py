71540c18ffc310858a44cf055895b824a241866b
Hamsik2rang <lvhi7121@gmail.com>
2021-02-24 09:01:54 +0900

    Day11 시간복잡도 분석 코드 Commit

--- Day11/Sources/Day11-2.py
+++ Day11/Sources/Day11-2.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-domain = [8 * i ** (1 / 2) for i in range(1, 1001)]
+domain_sqrt = [8 * (i ** (1 / 2)) for i in range(1, 1001)]
 domain_log = [math.log(i, 2) ** 2 for i in range(1, 1001)]
 domain_n = [i for i in range(1, 1001)]
-plt.plot(domain, "r--", domain_log, "b--", domain_n, "g--")
+plt.plot(domain_sqrt, "r--", domain_log, "b--", domain_n, "g--")
 plt.xlabel("R = 8*sqrt(2) / B = (log(n))^2 / G = n")
 plt.show()
