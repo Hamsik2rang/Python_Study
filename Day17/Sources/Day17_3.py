ea992e85242e86157e1340704120820898571e42
Im Yongsik <lvhi7121@gmail.com>
2021-03-02 23:28:31 +0900

    Converting from UTC to Local Time(KST) is Done!

--- Day17/Sources/Day17_3.py
+++ Day17/Sources/Day17_3.py
@@ -1,7 +1,8 @@
 import requests
 from datetime import datetime
+import time
 
-parameters = {"lat": 36.94010, "lng": 126.42809, "date": "today", "formatted": 0}
+parameters = {"lat": 12.34567, "lng": 123.45678, "date": "today", "formatted": 0}
 
 
 def raw_time_to_dict(raw):
@@ -13,6 +14,19 @@ def raw_time_to_dict(raw):
     return result
 
 
+def get_datetime(date, time):
+    result = datetime(date[0], date[1], date[2], time[0], time[1], time[2])
+    return result
+
+
+def from_UTC_to_local(utc):
+    now_local_timestamp = time.time()
+    offset = datetime.fromtimestamp(now_local_timestamp) - datetime.utcfromtimestamp(
+        now_local_timestamp
+    )
+    return utc + offset
+
+
 response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 # get data of UTC
@@ -21,25 +35,20 @@ sunrise = data["results"]["sunrise"]
 sunset = data["results"]["sunset"]
 
 sunrise_dict = raw_time_to_dict(sunrise)
-sunset_dixt = raw_time_to_dict(sunset)
+sunset_dict = raw_time_to_dict(sunset)
 
 sunrise_date = list(map(int, sunrise_dict["date"]))
 sunrise_time = list(map(int, sunrise_dict["time"]))
-sunset_date = list(map(int, sunrise_dict["date"]))
-sunset_time = list(map(int, sunrise_dict["time"]))
+sunset_date = list(map(int, sunset_dict["date"]))
+sunset_time = list(map(int, sunset_dict["time"]))
 
-utc_sunrise = datetime(
-    year=sunrise_date[0],
-    month=sunrise_date[1],
-    day=sunrise_date[2],
-    hour=sunrise_time[0],
-    minute=sunrise_time[1],
-    second=sunrise_time[2],
-)
+utc_sunrise = get_datetime(sunrise_date, sunrise_time)
+utc_sunset = get_datetime(sunset_date, sunset_time)
+
 # TODO: Convert UTC to KST. How can i do it?
-# print(utc_sunrise)
+# ----> It Works!
+local_sunrise = from_UTC_to_local(utc_sunrise)
+local_sunset = from_UTC_to_local(utc_sunset)
 
-# utc_sunrise_timestamp = utc_sunrise.strptime(utc_sunrise, "%Y-%M-%d %H:%M:%S")
-# print(utc_sunrise_timestamp)
-# local_sunrise = datetime.fromtimestamp(utc_sunrise_timestamp)
-# print(local_sunrise)
+print(f"Today's Sunrise Time : {local_sunrise}")
+print(f"Today's Sunset Time : {local_sunset}")
