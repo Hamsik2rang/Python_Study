431993d5d92741798e8fd3e69a347ff1c7b445d1
Im Yongsik <lvhi7121@gmail.com>
2021-03-04 23:39:32 +0900

    Day 19 commit - smtplib

--- Day19/Sources/Day19-1.py
+++ Day19/Sources/Day19-1.py
@@ -1,1 +1,25 @@
-# API using smtplib
+# Send Quote on monday using smtplib
+import random as rand
+from datetime import datetime
+import smtplib
+
+EMAIL = "lvhi7121@gmail.com"
+PASSWORD = "SECRET"
+MONDAY = 1
+
+with open("./Day19/Resources/quotes.txt", "r") as f:
+    quotes = f.readlines()
+
+    local_now = datetime.now()
+    weekday = local_now.weekday()
+    if weekday == MONDAY:
+        with smtplib.SMTP("smtp.gmail.com") as smtp:
+            smtp.starttls()
+            smtp.login(user=EMAIL, password=PASSWORD)
+            smtp.sendmail(
+                from_addr=EMAIL,
+                to_addrs="lvhi0607@naver.com",
+                msg=f"Subject:This Monday's Quote\n\n{rand.choice(quotes)}",
+            )
+    else:
+        pass
